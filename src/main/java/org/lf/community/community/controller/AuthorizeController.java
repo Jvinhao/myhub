92ec267d9d73630f16fa39573b19657bef7b725c
jwh <1340589637@qq.com>
2019-09-17 22:10:22 +0800

    整合mybatis

--- src/main/java/org/lf/community/community/controller/AuthorizeController.java
+++ src/main/java/org/lf/community/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package org.lf.community.community.controller;
 
 import org.lf.community.community.dto.AccessTokenDTO;
 import org.lf.community.community.dto.GithubUser;
+import org.lf.community.community.mapper.UserMapper;
+import org.lf.community.community.model.User;
 import org.lf.community.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,8 +11,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
 @Controller
 public class AuthorizeController {
+
+    @Autowired
+    UserMapper userMapper;
 
     @Autowired
     GithubProvider githubProvider;
@@ -25,7 +33,8 @@ public class AuthorizeController {
     private String redirectUri;
 
     @GetMapping("/callback")
-    public String callback(@RequestParam("code") String code, @RequestParam("state") String state) {
+    public String callback(@RequestParam("code") String code,
+                           @RequestParam("state") String state, HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setRedirect_uri(redirectUri);
@@ -33,8 +42,19 @@ public class AuthorizeController {
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser hubUser = githubProvider.getHubUser(accessToken);
-        System.out.println(hubUser.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getHubUser(accessToken);
+        //System.out.println(hubUser.getName());  验证
+        if(githubUser != null) {
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            //userMapper.insert(user);
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        }else {
+            return "redirect:/";
+        }
     }
 }
---
+++ src/main/java/org/lf/community/community/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package org.lf.community.community.mapper;
+
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.lf.community.community.model.User;
+
+@Mapper
+public interface UserMapper {
+
+    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    public void insert(User user);
+}
